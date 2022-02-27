b5dc6b15c508ad489c757d6791bd120952e53033
kalaiy21 <kalaiytz2132002@gmail.com>
2022-02-27 21:57:18 +0530

    changed table alignment

--- src/components/TotalAssets.jsx
+++ src/components/TotalAssets.jsx
@@ -2,11 +2,16 @@ import React from"react";
 
 
 function TotalAssets(){
-    return(<div className="mx-10 grid grid-cols-5  bg-[#141518]">
-    {/* <p className="text-white text-[1.5rem] font-light font-Inconsolate text-center m-5">Assets</p> */}
-    <p className="text-white text-[1.5rem] font-light font-Inconsolate text-center m-5">Total assets</p>
-    <p className= "w-full mx-10 text-white text-[1.5rem] font-light font-Inconsolate text-center border-2 m-4 bg-[#202023] border-[#8D795B] p-2 rounded-[10px]">$254,325,234,543</p>
-    </div>);
+    return(
+    <div className="mx-10 mb-8 bg-[#141518] pl-[5%]">
+        <div className="flex items-center gap-x-[2rem]">
+            <p className="text-white text-[1.5rem] font-light font-Inconsolate text-center">Total assets</p>
+            <div className="border-2 bg-[#202023] border-[#8D795B] p-2 rounded-[10px]">
+                <p className= "text-white text-[1.5rem] font-light font-Inconsolate text-center">$254,325,234,543</p>
+            </div>
+        </div>
+    </div>
+    );
 }
 
 
