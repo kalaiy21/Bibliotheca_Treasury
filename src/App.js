17808876fe0014f152002c23a1ff87c6f2f29435
kalaiy21 <kalaiytz2132002@gmail.com>
2022-04-10 13:20:57 +0530

    mb in heading and img sqaure fit

--- src/App.js
+++ src/App.js
@@ -69,13 +69,13 @@ function App() {
   return (
 
     <div className="py-[4rem] px-[8rem] tablet:px-[4rem] smphone:px-[2rem]">
-      <h1 className="text-center text-white title tracking-[4px] mb-[5rem] tablet:mb-[2.5rem] font-EB_Garamond standard:text-[6.5rem] desktop:text-[5.5rem] laptop:text-[4rem] minilap:text-[2.7rem] tablet:text-[1.8rem] phone:text-[1.5rem] smphone:text-[1.3rem] smphone:mb-[2rem]">
+      <h1 className="text-center text-white title tracking-[4px] laptop:mb-[5rem] tablet:mb-[2.5rem] font-EB_Garamond standard:text-[6.5rem] desktop:text-[5.5rem] laptop:text-[4rem] minilap:text-[2.7rem] tablet:text-[1.8rem] phone:text-[1.5rem] smphone:text-[1.3rem] smphone:mb-[2rem]">
 
         Bibliotheca <span className="text-[#8D795B] inline-block">DAO</span> <span className="inline-block">Treasury</span>
       </h1>
 
-      <div className="font-Inconsolata flex items-center justify-between mb-[5rem] tablet:flex-col-reverse laptop:flex-row phone:flex-col-reverse smphone:flex-col-reverse">
-        <div className="text-white font-light w-[60%] tablet:w-[100%] tracking-[2px] standard:text-[1.5rem] desktop:text-[1.4rem] desktop:text-[1.3rem] minilap:text-[1rem] tablet:text-[1.2rem] phone:text-[1rem] smphone:text-[0.8rem] phone:mt-[1rem] smphone:mt-[2rem]">
+      <div className="font-Inconsolata flex items-center justify-between mb-[5rem] tablet:flex-col-reverse laptop:flex-row phone:flex-col-reverse smphone:flex-col-reverse desktop:px-[2.5rem]">
+        <div className="text-white font-light laptop:w-[62%] tablet:w-[100%] tracking-[2px] standard:text-[1.5rem] desktop:text-[1.4rem] desktop:text-[1.3rem] minilap:text-[1rem] tablet:text-[1.2rem] phone:text-[1rem] smphone:text-[0.8rem] phone:mt-[1rem] smphone:mt-[2rem]">
         The treasury funds managed by community members will be used to enable the DAO's mission, vision and goals.
 
  
@@ -86,7 +86,7 @@ function App() {
           <li>real-time dashboard of state of the treasury</li>
         </ul>
         </div>
-        <img className="phone:p-2 standard:w-[27rem] standard:h-[25rem] desktop:w-[25rem] desktop:h-[23rem] laptopo:w-[23rem] laptop:h-[21rem] minilap:w-[19rem] minilap:h-[18rem] tablet:w-[15rem] tablet:h-[15rem] phone:w-[15rem] phone:h-[15rem] smphone:w-[13rem] smphone:h-[13rem] " src={logo} alt="treasury image" />
+        <img className="phone:p-2 standard:w-[25rem] standard:h-[25rem] desktop:w-[23rem] desktop:h-[23rem] laptopo:w-[21rem] laptop:h-[21rem] minilap:w-[18rem] minilap:h-[18rem] tablet:w-[15rem] tablet:h-[15rem] phone:w-[15rem] phone:h-[15rem] smphone:w-[13rem] smphone:h-[13rem] " src={logo} alt="treasury image" />
       </div>
 
       <Address address={walletaddress} />
